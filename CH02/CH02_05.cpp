3ce7363b3557cb90f4fbe18ccb45fe90e55c60aa
jay <jay.mm.message@gmail.com>
2024-01-01 01:08:48 +0800

    refactor code

--- CH02/CH02_05.cpp
+++ CH02/CH02_05.cpp
@@ -1,64 +1,72 @@
 #include <iostream>
 using namespace std;
 
-void MatrixMultiply(int*,int*,int*,int,int,int);
+void MatrixMultiply(int *, int *, int *, int, int, int);
 int main()
 {
-	int M,N,P;
-	int i,j;
-	//矩陣A部分 
- 	cout<<"請輸入矩陣A的維數(M,N): "<<endl;
- 	cout<<"M= ";
-	cin>>M;
-	cout<<"N= ";
-	cin>>N;
-	int *A = new int[M*N];
-	cout<<"[請輸入矩陣A的各個元素]"<<endl;
-	for(i=0;i<M;i++)
-		for(j=0;j<N;j++)
-		    {
-               cout<<"a"<<i<<j<<"=";
-			   cin>>A[i*N+j];
-            }
-	//矩陣B部分 
- 	cout<<"請輸入矩陣B的維數(N,P): "<<endl;
-	cout<<"N= ";
-	cin>>N;
-	cout<<"P= ";
-	cin>>P;
-	int *B = new int [N*P];
-	cout<<"[請輸入矩陣B的各個元素]"<<endl;
-	for(i=0;i<N;i++)
-		for(j=0;j<P;j++)
-        {
-           cout<<"b"<<i<<j<<"=";
-		   cin>>B[i*P+j];
-        }	
-	int *C = new int [M*P];
-	MatrixMultiply(A,B,C,M,N,P); //呼叫函數 
-	cout<<"[AxB的結果是]"<<endl;
-	for(i=0;i<M;i++)
+	int M, N, P;
+	int i, j;
+	// 矩陣A部分
+	cout << "Please enter the dimensions of matrix A(M,N): " << endl;
+	cout << "M= ";
+	cin >> M;
+	cout << "N= ";
+	cin >> N;
+	int *A = new int[M * N];
+	cout << "[Please enter the individual elements of matrix A]" << endl;
+	for (i = 0; i < M; i++)
+		for (j = 0; j < N; j++)
+		{
+			cout << "a" << i << j << "=";
+			cin >> A[i * N + j];
+		}
+	// 矩陣B部分
+	cout << "Please enter the dimensions of matrix B(N,P): " << endl;
+	cout << "N= ";
+	cin >> N;
+	cout << "P= ";
+	cin >> P;
+	int *B = new int[N * P];
+	cout << "[Please enter the individual elements of Matrix B]" << endl;
+	for (i = 0; i < N; i++)
+		for (j = 0; j < P; j++)
+		{
+			cout << "b" << i << j << "=";
+			cin >> B[i * P + j];
+		}
+	// M = 2;
+	// N = 3;
+	// int A[2][3] = {{5, 4, 3}, {6, 7, 8}};
+	// N = 3;
+	// P = 2;
+	// int B[3][2] = {{3, 4}, {6, 7}, {3, 6}};
+
+	int *C = new int[M * P];
+	MatrixMultiply(A, B, C, M, N, P); // 呼叫函數
+	//TMatrixMultiply(&A[0][0], &B[0][0], C, M, N, P); // 呼叫函數
+	cout << "[The result of A x B is]" << endl;
+	for (i = 0; i < M; i++)
 	{
-		for(j=0;j<P;j++)
-			cout<<C[i*P+j]<<"\t";
-		cout<<endl;
+		for (j = 0; j < P; j++)
+			cout << C[i * P + j] << "\t";
+		cout << endl;
 	}
 }
-//進行矩陣相乘 
-void MatrixMultiply(int* arrA,int* arrB,int* arrC,int M,int N,int P)
+// 進行矩陣相乘
+void MatrixMultiply(int *arrA, int *arrB, int *arrC, int M, int N, int P)
 {
-	if(M<=0||N<=0||P<=0)
+	if (M <= 0 || N <= 0 || P <= 0)
 	{
-		cout<<"[錯誤:維數M,N,P必須大於0]"<<endl;
+		cout << "[Error: Dimensions M, N, P must be greater than 0]" << endl;
 		return;
 	}
-	for(int i=0;i<M;i++)
-		for(int j=0;j<P;j++)
+	for (int i = 0; i < M; i++)
+		for (int j = 0; j < P; j++)
 		{
 			int Temp;
 			Temp = 0;
-			for(int k=0;k<N;k++)
-				Temp = Temp + arrA[i*N+k]*arrB[k*P+j];
-			arrC[i*P+j] = Temp;
+			for (int k = 0; k < N; k++)
+				Temp = Temp + arrA[i * N + k] * arrB[k * P + j];
+			arrC[i * P + j] = Temp;
 		}
 }
