435e1e64f16435b6d62b17934c51ef0891773fe4
jay <jay.mm.message@gmail.com>
2024-01-01 02:08:56 +0800

    refactor code

--- CH02/CH02_06.cpp
+++ CH02/CH02_06.cpp
@@ -3,45 +3,45 @@ using namespace std;
 
 int main()
 {
-	int M,N,row,col;
-	cout<<"[輸入MxN矩陣的維度]"<<endl;
-	cout<<"請輸入維度M: ";
-	cin>>M;
-	cout<<"請輸入維度N: ";
-	cin>>N;
-	
-   	int *arrA = new int[M*N];//宣告動態陣列 
-	int *arrB = new int[M*N];
- 	cout<<"[請輸入矩陣內容]"<<endl;
-	for(row=1;row<=M;row++)
-	{
-		for(col=1;col<=N;col++)
-		{
-			cout<<"a"<<row<<col<<"=";
-			cin>>arrA[(row-1)*N+(col-1)];
-		}
-	}
-	cout<<"[輸入矩陣內容為]"<<endl;
-	for(row=1;row<=M;row++)
-	{
-		for(col=1;col<=N;col++)
-		{
-			cout<<arrA[(row-1)*N+(col-1)]<<"\t";
-		}
-		cout<<endl;
-	}
-	//進行矩陣轉置的動作
-	for(row=1;row<=N;row++)
-		for(col=1;col<=M;col++)
-			arrB[(col-1)*N+(row-1)]=arrA[(row-1)+(col-1)*N];
+	int M, N, row, col;
+	cout << "[Enter the dimensions of the MxN matrix]" << endl;
+	cout << "Please enter the dimension M: ";
+	cin >> M;
+	cout << "Please enter the dimension N: ";
+	cin >> N;
 
-	cout<<"[轉置矩陣內容為]"<<endl;
-	for(row=1;row<=N;row++)
+	int *arrA = new int[M * N]; // 宣告動態陣列
+	int *arrB = new int[M * N];
+	cout << "[Please enter the matrix content]" << endl;
+	for (row = 1; row <= M; row++)
 	{
-		for(col=1;col<=M;col++)
+		for (col = 1; col <= N; col++)
 		{
-			cout<<arrB[(col-1)*N+(row-1)]<<"\t";
+			cout << "a" << row << col << "=";
+			cin >> arrA[(row - 1) * N + (col - 1)];
 		}
-		cout<<endl;
+	}
+	cout << "[Enter the matrix content as]" << endl;
+	for (row = 1; row <= M; row++)
+	{
+		for (col = 1; col <= N; col++)
+		{
+			cout << arrA[(row - 1) * N + (col - 1)] << "\t";
+		}
+		cout << endl;
+	}
+	// 進行矩陣轉置的動作
+	for (row = 1; row <= N; row++)
+		for (col = 1; col <= M; col++)
+			arrB[(col - 1) * N + (row - 1)] = arrA[(row - 1) + (col - 1) * N];
+
+	cout << "[Transpose the matrix content as]" << endl;
+	for (row = 1; row <= N; row++)
+	{
+		for (col = 1; col <= M; col++)
+		{
+			cout << arrB[(col - 1) * N + (row - 1)] << "\t";
+		}
+		cout << endl;
 	}
 }
