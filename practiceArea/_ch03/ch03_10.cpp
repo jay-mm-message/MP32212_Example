0ce678a2c40fb6b845ccdf0da49cac0cd98b384d
jay <jay.mm.message@gmail.com>
2024-01-15 14:26:18 +0800

    Add three mode (head, tail and middle) to the circular linked list

---
+++ practiceArea/_ch03/ch03_10.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+using namespace std;
+
+class node {
+    public:
+        int num;
+        char letters[2];
+    class node *next;
+};
+
+typedef node *link;
+
+link init_cycle_link(link head, int count) {
+    link p = NULL;
+
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+        link newNode = new node;
+        newNode->num = i + 1;
+        newNode->letters[0] = 'A' + i;
+        newNode->letters[1] = '\0';
+        newNode->next = NULL;
+
+        if (head == NULL)
+        {
+            /* code */
+            head = newNode;
+            p = head;
+        }
+        else
+        {
+            p->next = newNode;
+            p = p->next;
+        }
+    } // for 
+    p->next = head;
+    return head;
+}
+
+void show_cycle(link head) {
+
+    if (head == NULL)
+    {
+        /* code */
+        cout << "Link is empty" << endl;
+        return;
+    }
+    
+    link p = head;
+
+    cout << p->num << ":" << p->letters << ' ';
+    p = p->next;
+    while (p != head)
+    {
+        /* code */
+        cout << p->num << ":" << p->letters << ' ';
+        p = p->next;
+    }
+    cout << endl;
+}
+
+link insert_node(link head, int num, link data) {
+    
+    link p = head;
+    link newNode = new node;
+    newNode->num = data->num;
+    strcpy(newNode->letters, data->letters);
+
+    if (num == 0)
+    {
+        /* code */
+        link tail = head;
+        while (tail->next != head)
+        {
+            /* code */
+            tail = tail->next;
+        }
+        tail->next = newNode;
+        newNode->next = head;
+        return newNode;
+    }
+    p = p->next;
+
+    while(p != head) {
+        if (p->num == num)
+        {
+            /* code */
+            newNode->next = p->next;
+            p->next = newNode;   
+            return head; 
+        }
+        p = p->next;
+    }
+
+    newNode->next = p->next;
+    p->next = newNode;
+
+    return head;
+}
+
+link findNode(link head, const char *letters) {
+    link p = head;
+
+    if (strcmp(p->letters, letters) == 0)
+    {
+        /* code */
+        return head;        
+    }
+    p = p->next;
+    while(p != head) {
+        if (strcmp(p->letters, letters) == 0)
+        {
+            /* code */
+            return p;
+        }
+        
+        p = p->next;
+    }
+    return NULL;
+}
+
+link delNode(link head, const char *letters) {
+    // findNode and remove
+    link dnode = findNode(head, letters);
+
+    link p = head;
+
+    if (dnode == p)
+    {
+        /* code */
+        while (p->next != head)
+        {
+            /* code */
+            p = p->next;
+        }
+        p->next = head->next;
+        head = head->next;
+        dnode->next = NULL;
+        cout << "remove dnode: " << dnode->num << ":" << dnode->letters << endl;
+        delete dnode;
+    }
+    
+    while (p->next != head)
+    {
+        /* code */
+        if (p->next == dnode)
+        {
+            /* code */
+            p->next = p->next->next;
+            dnode->next = NULL;
+            cout << "remove dnode: " << dnode->num << ":" << dnode->letters << endl;
+            delete dnode;
+        }
+        
+        p = p->next;
+    }
+
+    return head;
+}
+
+int main(void) {
+
+    link head = NULL;
+    // init_cycle_link
+    head = init_cycle_link(head, 5);
+    // show_cycle
+    show_cycle(head);
+
+    link newNode = new node;
+    newNode->num = 100;
+    strcpy(newNode->letters, "Z");
+    head = insert_node(head, 0, newNode);
+    show_cycle(head);
+    newNode->num = 200;
+    strcpy(newNode->letters, "Y");
+    head = insert_node(head, 1, newNode);
+    show_cycle(head);
+    newNode->num = 300;
+    strcpy(newNode->letters, "X");
+    head = insert_node(head, 5, newNode);
+    show_cycle(head);
+
+    const char *findWord = "X";
+    link pFind = findNode(head, findWord);
+    if (pFind == NULL)
+    {
+        /* code */
+        cout << "Not found: " << findWord << endl;
+    }
+    else
+    {
+        cout << pFind->num << ":" << pFind->letters << ' ';
+    }
+    cout << endl;
+
+    cout << "remove head" << endl;
+    head = delNode(head, "Z");
+    show_cycle(head);
+    cout << "remove middle" << endl;
+    head = delNode(head, "Y");
+    show_cycle(head);
+    cout << "remove tail" << endl;
+    head = delNode(head, "X");
+    show_cycle(head);
+
+    return 0;
+}
