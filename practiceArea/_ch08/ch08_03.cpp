f7f524bcd3482be8683a72cf55179e3d8603a58f
jay <jay.mm.message@gmail.com>
2024-01-24 02:12:34 +0800

    Refactor

--- practiceArea/_ch08/ch08_03.cpp
+++ practiceArea/_ch08/ch08_03.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void swap(int &a, int &b);
 void selection_sort(vector<int> &vec, int sort_mode);
-void show(vector<int> vec, int sort_mode);
+void show(vector<int> vec);
 void init_vec(vector<int> &vec, int count);
 void insertion_sort(vector<int> &vec, int sort_mode);
 void merge_sort(vector<int> &vec, int l, int r, int sort_mode);
@@ -18,28 +18,28 @@ int main() {
    
     vector<int> vec;
     init_vec(vec, 5);
-    show(vec,no_sort);
+    show(vec);
     
     selection_sort(vec, sort_from_small_to_large);
-    show(vec, sort_from_small_to_large);
+    show(vec);
 
     insertion_sort(vec, sort_from_large_to_smal);
-    show(vec, sort_from_large_to_smal);
+    show(vec);
 
     cout << "merge sort" << endl;
     merge_sort(vec, 0, vec.size() - 1, sort_from_small_to_large);
-    show(vec, sort_from_small_to_large);
+    show(vec);
     
 
     selection_sort(vec, sort_from_large_to_smal);
-    show(vec, sort_from_large_to_smal);
+    show(vec);
 
     insertion_sort(vec, sort_from_small_to_large);
-    show(vec, sort_from_small_to_large);
+    show(vec);
 
     cout << "merge sort" << endl;
     merge_sort(vec, 0, vec.size() - 1, sort_from_large_to_smal);
-    show(vec, sort_from_large_to_smal);
+    show(vec);
 
     return 0;
 }
@@ -65,6 +65,7 @@ void selection_sort(vector<int> &vec, int sort_mode) {
     const int sort_from_small_to_large = 1;
     const int sort_from_large_to_smal  = 2; */
     // selection vec for all - 1
+    string log_msg = "";
     for (size_t j = 0; j < vec.size() - 1; j++)
     {
         /* code */
@@ -75,6 +76,7 @@ void selection_sort(vector<int> &vec, int sort_mode) {
             /* code */
             if (sort_from_small_to_large == sort_mode)
             {
+                log_msg = "sort_from_small_to_large";
                 /* code */
                 if (vec[index] > vec[i])
                 {
@@ -85,6 +87,7 @@ void selection_sort(vector<int> &vec, int sort_mode) {
 
             if (sort_from_large_to_smal == sort_mode)
             {
+                log_msg = "sort_from_large_to_smal";
                 /* code */
                 if (vec[index] < vec[i])
                 {
@@ -97,23 +100,10 @@ void selection_sort(vector<int> &vec, int sort_mode) {
         swap(vec[index], vec[j]);
     }
 
-    cout << "selection sort finish" << endl;
+    cout << "selection sort finish: " << log_msg << endl;
 }
 
-void show(vector<int> vec, int sort_mode) {
-
-    switch (sort_mode)
-    {
-    case sort_from_large_to_smal:
-        cout << "sort_from_large_to_smal" << endl;
-        break;
-     case sort_from_small_to_large:
-        cout << "sort_from_small_to_large" << endl;
-        break;   
-    default:
-        cout << "no sort" << endl;
-        break;
-    }
+void show(vector<int> vec) {
 
     for (size_t i = 0; i < vec.size(); i++)
     {
@@ -127,6 +117,8 @@ void insertion_sort(vector<int> &vec, int sort_mode) {
     /*
     const int sort_from_small_to_large = 1;
     const int sort_from_large_to_smal  = 2; */
+
+    string log_msg = "";
     for (size_t i = 1; i < vec.size(); i++)
     {
         /* code */
@@ -136,7 +128,8 @@ void insertion_sort(vector<int> &vec, int sort_mode) {
 
         if (sort_from_large_to_smal == sort_mode)
         {
-            while (bk >= 0 && vec[bk] < key)
+            log_msg = "sort_from_large_to_smal";
+            while (bk >= 0 && key > vec[bk])
             {
                 /* code */
                 vec[bk+1] = vec[bk];
@@ -146,7 +139,8 @@ void insertion_sort(vector<int> &vec, int sort_mode) {
 
         if (sort_from_small_to_large == sort_mode)
         {
-            while (bk >= 0 && vec[bk] > key)
+            log_msg = "sort_from_small_to_large";
+            while (bk >= 0 && key < vec[bk])
             {
                 /* code */
                 vec[bk+1] = vec[bk];
@@ -158,7 +152,7 @@ void insertion_sort(vector<int> &vec, int sort_mode) {
 
         vec[bk + 1] = key;
     }
-    cout << "insertion sort finish" << endl;
+    cout << "insertion sort finish: " << log_msg << endl;
 }
 
 void merge_sort(vector<int> &vec, int l, int r, int sort_mode) {
