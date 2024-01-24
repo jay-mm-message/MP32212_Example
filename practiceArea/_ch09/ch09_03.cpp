59cafbf37b35350163b60835e21031d810f88d64
jay <jay.mm.message@gmail.com>
2024-01-25 00:03:40 +0800

    Add interpolation_search && refactor binary search

---
+++ practiceArea/_ch09/ch09_03.cpp
@@ -0,0 +1,215 @@
+#include <iostream>
+using namespace std;
+
+void init_vec(vector<int> &vec, int count);
+void show(vector<int> &vec);
+void selection_sort(vector<int> &vec);
+void linear_search(vector<int> &vec, int foundNum);
+void binary_search(vector<int> &vec, int foundNum);
+void interpolation_search(vector<int> &vec, int foundNum);
+// mid=low+((val-data[low])*(high-low)/(data[high]-data[low]));
+
+const int BSR = 1;
+const int LSR = 2;
+const int ISR = 3;
+
+int main()
+{
+    vector<int> vec(0);
+    init_vec(vec, 80);
+    show(vec);
+    // linear_search(vec, 8);
+
+    selection_sort(vec);
+    show(vec);
+
+    int search_mode = 0, search_number = 0;
+    cout << "Please enter search mode:"
+         << "\n(1) binary search, (2) linear search (3) interpolation_search ";
+    cin >> search_mode;
+    cout << "Please enter search number: ";
+    cin >> search_number;
+
+    switch (search_mode)
+    {
+    case BSR:
+        binary_search(vec, search_number);
+        break;
+    case LSR:
+        linear_search(vec, search_number);
+        break;
+    case ISR:
+        interpolation_search(vec, search_number);
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
+
+void show(vector<int> &vec)
+{
+    cout << "vec: ";
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        /* code */
+        cout << vec[i] << ' ';
+    }
+    cout << endl
+         << endl;
+}
+
+void init_vec(vector<int> &vec, int count)
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+        vec.push_back(rand() % 150 + 1);
+    }
+}
+
+void swap(int &a, int &b)
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void selection_sort(vector<int> &vec)
+{
+    for (size_t i = 0; i < vec.size() - 1; i++)
+    {
+        /* code */
+        int min_index = i;
+        for (size_t j = i + 1; j < vec.size(); j++)
+        {
+            /* code */
+            if (vec[min_index] > vec[j])
+            {
+                /* code */
+                min_index = j;
+            }
+        }
+        swap(vec[min_index], vec[i]);
+    }
+}
+
+void linear_search(vector<int> &vec, int foundNum)
+{
+    // int foundNum = 1;
+    cout << __FUNCTION__ << ":" << ' ';
+    bool isFound = false;
+    int searchCount = 1;
+
+    for (size_t i = 0; i < 80; i++)
+    {
+        /* code */
+        if (vec[i] == foundNum)
+        {
+            /* code */
+            isFound = true;
+            break;
+        }
+        /* code */
+        ++searchCount;
+    }
+
+    if (isFound)
+    {
+        /* code */
+        cout << "Found number: " << foundNum << ", search cost: " << searchCount << endl;
+    }
+    else
+    {
+        cout << "Not found number: " << foundNum << ", search cost: " << searchCount << endl;
+    }
+}
+
+void binary_search(vector<int> &vec, int foundNum)
+{
+    cout << __FUNCTION__ << ":" << ' ';
+    int search_cost = 1;
+    int l = 0;
+    int r = vec.size();
+    
+    bool isFound = false;
+    while (l <= r && foundNum != -1)
+    {
+        int mid = (l + r) / 2;
+
+        /* code */
+        if (foundNum > vec[mid])
+        {
+            /* code */
+            l = mid + 1;
+            search_cost++;
+        }
+        else if (foundNum < vec[mid])
+        {
+            r = mid - 1;
+            search_cost++;
+        }
+        else if (foundNum == vec[mid])
+        {
+            isFound = true;
+            break;
+        }
+    }
+    if (isFound)
+    {
+        /* code */
+        cout << "Found num: " << foundNum << ", search cost: " << search_cost << endl;
+    }
+    else
+    {
+        cout << "Not found num: " << foundNum << endl;
+    }
+}
+
+void interpolation_search(vector<int> &vec, int foundNum)
+{
+    cout << __FUNCTION__ << ":" << ' ';
+    int search_cost = 1;
+    int l = 0;
+    int r = vec.size();
+    int low = l;
+    int high = r;
+    int val = foundNum;
+
+    ////mid=low+((val-data[low])*(high-low)/(data[high]-data[low]));
+    //int mid = low + ((val - vec[low]) * (high - low) / (vec[high] - vec[low]));
+    
+    bool isFound = false;
+    while (l <= r && foundNum != -1)
+    {
+        int mid = low + ((val - vec[low]) * (high - low) / (vec[high] - vec[low]));
+
+        /* code */
+        if (foundNum > vec[mid])
+        {
+            /* code */
+            l = mid + 1;
+            search_cost++;
+        }
+        else if (foundNum < vec[mid])
+        {
+            r = mid - 1;
+            search_cost++;
+        }
+        else if (foundNum == vec[mid])
+        {
+            isFound = true;
+            break;
+        }
+    }
+    if (isFound)
+    {
+        /* code */
+        cout << "Found num: " << foundNum << ", search cost: " << search_cost << endl;
+    }
+    else
+    {
+        cout << "Not found num: " << foundNum << endl;
+    }
+}
